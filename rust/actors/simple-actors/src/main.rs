8f2cae22fc35d576e6fb23261b82b7cf32d6bbf1
Ægir Örn Símonarson <agirorn@gmail.com>
2023-11-13 15:18:55 +0000

    Refactored simple actor

--- rust/actors/simple-actors/src/main.rs
+++ rust/actors/simple-actors/src/main.rs
@@ -1,10 +1,10 @@
 use tokio::sync::{mpsc, oneshot};
 
-struct MyActor {
-    receiver: mpsc::Receiver<ActorMessage>,
+struct Actor {
+    receiver: mpsc::Receiver<Message>,
     next_id: u32,
 }
-pub enum ActorMessage {
+pub enum Message {
     GetUniqueId {
         respond_to: oneshot::Sender<u32>,
     },
@@ -12,17 +12,17 @@ pub enum ActorMessage {
     Shutdown,
 }
 
-impl MyActor {
-    fn new(receiver: mpsc::Receiver<ActorMessage>) -> Self {
-        MyActor {
+impl Actor {
+    fn new(receiver: mpsc::Receiver<Message>) -> Self {
+        Actor {
             receiver,
             next_id: 0,
         }
     }
 
-    async fn handle_message(&mut self, msg: ActorMessage) {
+    async fn handle_message(&mut self, msg: Message) {
         match msg {
-            ActorMessage::GetUniqueId { respond_to } => {
+            Message::GetUniqueId { respond_to } => {
                 self.next_id += 1;
 
                 // The `let _ =` ignores any errors when sending.
@@ -31,29 +31,29 @@ impl MyActor {
                 // to cancel waiting for the response.
                 let _ = respond_to.send(self.next_id);
             }
-            ActorMessage::Shutdown => {
+            Message::Shutdown => {
                 self.receiver.close();
             }
         }
     }
 }
 
-async fn run_my_actor(mut actor: MyActor) {
+async fn run_my_actor(mut actor: Actor) {
     while let Some(msg) = actor.receiver.recv().await {
         actor.handle_message(msg).await;
     }
 }
 
 #[derive(Clone)]
-pub struct MyActorHandle {
-    sender: mpsc::Sender<ActorMessage>,
+pub struct Handle {
+    sender: mpsc::Sender<Message>,
 }
 
-impl MyActorHandle {
+impl Handle {
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         let (sender, receiver) = mpsc::channel(8);
-        let actor = MyActor::new(receiver);
+        let actor = Actor::new(receiver);
         tokio::spawn(run_my_actor(actor));
         Self { sender }
     }
@@ -61,7 +61,7 @@ impl MyActorHandle {
     pub async fn get_unique_id(&self) -> u32 {
         let (respond_to, recv) = oneshot::channel();
         self.sender
-            .send(ActorMessage::GetUniqueId { respond_to })
+            .send(Message::GetUniqueId { respond_to })
             .await
             .unwrap();
         recv.await.expect("Actor task has been killed")
@@ -69,7 +69,7 @@ impl MyActorHandle {
 
     /// Sending a cose messages to the Actor so it can close the reciver and shutdown propperly.
     pub async fn close(&self) -> Result<()> {
-        self.sender.send(ActorMessage::Shutdown).await.unwrap();
+        self.sender.send(Message::Shutdown).await.unwrap();
         futures::join!(self.sender.closed());
         Ok(())
     }
@@ -77,16 +77,15 @@ impl MyActorHandle {
 
 use anyhow::Result;
 
-// fn main() {
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Hello, world!");
-    let handle = MyActorHandle::new();
-    println!("Unique id: {}", handle.get_unique_id().await);
-    println!("Unique id: {}", handle.get_unique_id().await);
-    println!("Unique id: {}", handle.get_unique_id().await);
-    println!("Unique id: {}", handle.get_unique_id().await);
-    handle.close().await.unwrap();
+    let actor = Handle::new();
+    println!("Unique id: {}", actor.get_unique_id().await);
+    println!("Unique id: {}", actor.get_unique_id().await);
+    println!("Unique id: {}", actor.get_unique_id().await);
+    println!("Unique id: {}", actor.get_unique_id().await);
+    actor.close().await.unwrap();
 
     Ok(())
 }
